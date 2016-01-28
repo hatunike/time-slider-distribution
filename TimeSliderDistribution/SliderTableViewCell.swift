fe74ffa20f7ccfe874a066be0b763a69c55b41a4
Charles Hart <charleshart.lds@gmail.com>
2016-01-27 23:20:06 -0700

    add cell subclass

---
+++ TimeSliderDistribution/SliderTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SliderTableViewCell.swift
+//  TimeSliderDistribution
+//
+//  Created by Charles Hart on 1/27/16.
+//  Copyright © 2016 lernu. All rights reserved.
+//
+
+import UIKit
+
+class SliderTableViewCell: UITableViewCell {
+
+    @IBOutlet var timeSlider: UISlider!
+    @IBOutlet var timeTotalLabel: UILabel!
+    
+}
